f8ce235197e8cf65bb03cc09569cf2d49fced33b
fastlearner1110 <95926942+fastlearner1110@users.noreply.github.com>
2022-02-23 23:13:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,11 @@ const App = () => {
   return (
     <div id="main">
       <div data-ns-test="project-name">TIME VARYING INTERACTION LEADS TO AMPLITUDE DEATH IN COUPLED OSCILLATORS</div>
-      <p data-ns-test="project-description">the behaviour of two
+      <div data-ns-test="project-description">the behaviour of two
 political parties and the impact of one on the other and analysing whether the behaviour is
 time varying or space varying.I have also analysed the coupled Landu-Stuart Oscillator and
 Lorenz Oscillator and have shown how TIME VARYING INTERACTION LEADS TO AMPLITUDE
-DEATH IN COUPLED OSCILLATORS</p>
+DEATH IN COUPLED OSCILLATORS</div>
     </div>
   )
 }
